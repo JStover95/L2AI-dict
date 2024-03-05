42ecf96fcf5b1cd7abfc42d02180fda5d1f739c1
Jesse Stover <j.stover.r@gmail.com>
2024-03-05 20:46:38 +0900

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # L2AI-dict
+
+This is a tool for demonstrating how to use my [L2AI-dictionary model](https://huggingface.co/JesseStover/L2AI-dictionary-klue-bert-base) that is a fine-tuned checkpoint of [klue/bert-base](https://huggingface.co/klue/bert-base).
+
+**Note:** This script must be run in a Python 3.12 environment with PyTorch. Refer to [this link](https://pytorch.org/get-started/locally/) for installing PyTorch locally.
+
+## Quickstart
+
+On Linux, run `./deploy.sh` to initialize a Docker container with an empty MongoDB container and store the contents of `dict.json` in MongoDB.
+
+On Windows, manually create a new MongoDB container. Then, open Python in a command line and run the following commands:
+
+```
+>>> from dictionary import create_db
+>>> create_db()
+```
+
+Then, run the `infer.py` script from the command line and provide a query:
+
+```
+python infer.py "강아지는 뽀송뽀송하다."
+```
+
+English definitions of each word in the supplied query, and their respective scores from 0 to 1 (1 being the highest confidence that the definition is correct), will be printed to the terminal. An example output will be:
+
+```
+강아지
+0.9999988079071045 puppy: The pup of a dog.
+1.2131376934121363e-06 baby; sweetheart: A word of endearment used to refer to or address one's child or grandchild.
+
+뽀송뽀송
+0.9997172951698303 fuzzily; downily: In the state of having very short, soft hair or something similar.
+0.0002767054829746485 in a dry and tender state: In the state of being dried yet soft. 
+5.952259471087018e-06 smoothly; softly: In a state in which one's skin or face is soft and smooth.
+```
+
+Alternatively, you can import `get_inference` from `infer.py` for use in other modules.
+
+## L2AI-dictionary
+
+The L2AI-dictionary model is fine-tuned for multiple choice, specifically for selecting the best dictionary definition of a given word in a sentence. Below is an example usage:
+
+```python
+import numpy as np
+import torch
+from transformers import AutoModelForMultipleChoice, AutoTokenizer
+
+model_name = "JesseStover/L2AI-dictionary-klue-bert-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForMultipleChoice.from_pretrained(model_name)
+model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+prompts = "\"강아지는 뽀송뽀송하다.\"에 있는 \"강아지\"의 정의는 "
+candidates = [
+    "\"(명사) 개의 새끼\"예요.",
+    "\"(명사) 부모나 할아버지, 할머니가 자식이나 손주를 귀여워하면서 부르는 말\"이예요."
+]
+
+inputs = tokenizer(
+    [[prompt, candidate] for candidate in candidates],
+    return_tensors="pt",
+    padding=True
+)
+
+labels = torch.tensor(0).unsqueeze(0)
+
+with torch.no_grad():
+    outputs = model(
+        **{k: v.unsqueeze(0) for k, v in inputs.items()}, labels=labels
+    )
+
+print({i: float(x) for i, x in enumerate(outputs.logits.softmax(1)[0])})
+```
+
+Training data was procured under Creative Commons [CC BY-SA 2.0 KR DEED](https://creativecommons.org/licenses/by-sa/2.0/kr/) from the National Institute of Korean Language's [Basic Korean Dictionary](https://krdict.korean.go.kr) and [Standard Korean Dictionary](https://stdict.korean.go.kr/).
--- infer.py
+++ infer.py
@@ -1,10 +1,24 @@
 from functools import reduce
 import re
 import sys
+from typing import Tuple
 import jamotools
 import torch
+from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForMultipleChoice
 from dictionary import query_dictionary, get_query_str
+
+model_name = "JesseStover/L2AI-dictionary-klue-bert-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForMultipleChoice.from_pretrained(model_name)
+model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+
+type InferenceResult = dict[str, list[Tuple[float, str]]]
+exclude_words = ["것", "수", "있다", "안", "하다", "되다"]
+
+
+def b(s) -> str:
+    return "\033[94m%s\033[0m" % str(s)
 
 
 def ends_in_vowel(str: str) -> bool:
@@ -17,83 +31,94 @@ def ends_in_vowel(str: str) -> bool:
     return ord(jamos[-1]) >= 0x1161 and ord(jamos[-1]) <= 0x1175
 
 
-model_name = "JesseStover/L2AI-dictionary-klue-bert-base"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForMultipleChoice.from_pretrained(model_name)
-model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+def get_inference(query: str) -> InferenceResult:
+    groups = query_dictionary(query)
+    qstr = get_query_str(query)
+    result = {}
 
-try:
-    query = sys.argv[1]
-except IndexError:
-    print("Usage: %s \"<query>\"" % sys.argv[0])
-    quit()
+    for group in tqdm(groups):
+        query_strs = group[0]["queryStrs"]
+        variations = group[0]["variations"]
 
-groups = query_dictionary(query)
-qstr = get_query_str(query)
-words = []
+        if variations[0] in exclude_words:
+            continue
 
-for group in groups:
-    query_strs = group[0]["queryStrs"]
-    variations = group[0]["variations"]
+        i = 0
+        while i < len(query_strs) and query_strs[i] not in qstr:
+            i = i + 1
+        if i == len(query_strs):
+            msg ="%s not found in query %s" % (group[0]["variations"][0], qstr)
+            raise ValueError(msg)
 
-    i = 0
-    while i < len(query_strs) and query_strs[i] not in qstr:
-        i = i + 1
-    if i == len(query_strs):
-        msg ="%s not found in query %s" % (group[0]["variations"][0], qstr)
-        raise ValueError(msg)
+        word = variations[i]
+        prompt = "\"%s\"에 있는 \"%s\"의 정의는 " % (query, word)
 
-    word = variations[i]
-    prompt = "\"%s\"에 있는 \"%s\"의 정의는 " % (query, word)
-
-    senses = reduce(
-        lambda l, r: (
-            l + ["(%s) %s" % (r["partOfSpeechKo"], x) for x in r["sensesKo"]]
-        ),
-        group,
-        []
-    )
-
-    if len(senses) == 1:
-        result = [1.0]
-
-    else:
-        candidates = []
-
-        for sense in senses:
-            if sense.endswith("."):
-                sense = sense[:-1]
-
-            sense_stripped = re.sub(r"[^\u3131-\uD79DA-Za-z\d]", "", sense)
-            end = "예요." if ends_in_vowel(sense_stripped) else "이에요."
-            candidates.append("\"%s\"%s" % (sense, end))
-
-        inputs = tokenizer(
-            [[prompt, candidate] for candidate in candidates],
-            return_tensors="pt",
-            padding=True
+        senses = reduce(
+            lambda l, r: (
+                l + [
+                    "(%s) %s" % (r["partOfSpeechKo"], x) for x in r["sensesKo"]
+                ]
+            ),
+            group,
+            []
         )
 
-        labels = torch.tensor(0).unsqueeze(0)
+        if len(senses) == 1:
+            infer_result = [1.0]
 
-        with torch.no_grad():
-            outputs = model(
-                **{k: v.unsqueeze(0) for k, v in inputs.items()},
-                labels=labels
+        else:
+            candidates = []
+
+            for sense in senses:
+                if sense.endswith("."):
+                    sense = sense[:-1]
+
+                sense_stripped = re.sub(r"[^\u3131-\uD79DA-Za-z\d]", "", sense)
+                end = "예요." if ends_in_vowel(sense_stripped) else "이에요."
+                candidates.append("\"%s\"%s" % (sense, end))
+
+            inputs = tokenizer(
+                [[prompt, candidate] for candidate in candidates],
+                return_tensors="pt",
+                padding=True
             )
 
-        result = [float(x) for x in outputs.logits.softmax(1)[0]]
+            labels = torch.tensor(0).unsqueeze(0)
 
-    start = 0
-    print(group[0]["variations"])
-    for word in group:
-        end = start + len(word["sensesKo"])
-        word["senseScores"] = result[start:end]
-        start = end
-        ranks = list(zip(word["sensesEn"], word["senseScores"]))
-        ranks = sorted(ranks, key=lambda x: x[1])
+            with torch.no_grad():
+                outputs = model(
+                    **{k: v.unsqueeze(0) for k, v in inputs.items()},
+                    labels=labels
+                )
 
-        for sense, rank in reversed(ranks):
-            print(rank, sense)
+            infer_result = [float(x) for x in outputs.logits.softmax(1)[0]]
 
-    print()
+        start = 0
+        ranks = []
+        for word in group:
+            end = start + len(word["sensesKo"])
+            word["senseScores"] = infer_result[start:end]
+            start = end
+            ranks.extend(list(zip(word["senseScores"], word["sensesEn"])))
+
+        ranks.sort(key=lambda x: x[0], reverse=True)
+        result[variations[0]] = ranks
+
+    return result
+
+
+if __name__ == "__main__":
+    try:
+        query = sys.argv[1]
+        result = get_inference(query)
+
+        for word, ranks in result.items():
+            print(word)
+
+            for rank, sense in ranks:
+                print(b(rank), sense)
+
+            print()
+
+    except IndexError:
+        print("Usage: %s \"<query>\"" % sys.argv[0])
